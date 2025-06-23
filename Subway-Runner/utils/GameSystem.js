a4ba34caf889a5903a6c51aee085b737639a07e7
James <118614403+JumesP@users.noreply.github.com>
2025-06-23 15:36:36 +0100

    most functionality moved to classes

--- Subway-Runner/utils/GameSystem.js
+++ Subway-Runner/utils/GameSystem.js
@@ -3,35 +3,25 @@ const GameSystem = (endGame, increaseScore) => (entities, { touches, time }) => 
     let obstacles = Object.values(entities).filter(entity => entity.type === 'obstacle');
 
     obstacles.forEach(obstacle => {
-        obsticle.position[1] += 5; // Move obstacles down the screen
+        obstacle.moveDown()
         if (obstacle.position[1] > 600) { // bottom of the screen
-            // Reset obstacle position to the top
-            obstacle.position[1] = -50;
-            obstacle.lane = Math.floor(Math.random() * 3); // Randomize lane
-            //update obstacle type
             increaseScore(); // Update score when obstacle resets
         }
-
-        // Adjust obstacle position based on lane
-        obstacle.position[0] = obstacle.lane * 100 + 50;
     });
 
     // Check for collisions
     obstacles.forEach(obstacle => {
-        if (
-            player.lane === obstacle.lane &&
-            obstacle.position[1] > player.position[1] &&
-            obstacle.position[1] < player.position[1] + player.size[1]
-        ) {
+        const checkCollision = obstacle.detectCollision(player);
+        if (checkCollision) {
             endGame(); // End game if collision occurs
         }
     });
 
     // Handle lane changes based on touches
-    touches.filter(t => t.type === 'press').forEach(t -> {
-        if (t.event.pageX < 150) {player.lane = 0} // Move Left
-        else if (t.event.pageX > 250) {player.lane = 2;} // Move Right
-        else {player.lane = 1;} // Center lane
+    touches.filter(t => t.type === 'press').forEach(t => {
+        if (t.event.pageX < 150) {player.moveToLeftLane()} // Move Left
+        else if (t.event.pageX > 250) {player.moveToRightLane()} // Move Right
+        else {player.moveToMiddleLane();} // Center lane
     })
 
     player.position[0] = player.lane * 100 + 50; // Update player position based on lane
@@ -39,4 +29,4 @@ const GameSystem = (endGame, increaseScore) => (entities, { touches, time }) => 
     return entities;
 };
 
-export GameSystem;
+export default GameSystem;
